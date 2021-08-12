ea51f7739e8445d5b0303a9b8c674c848845caaf
root <root@ip-172-26-7-37.ap-northeast-2.compute.internal>
2021-08-12 18:35:46 +0000

    please

--- firstproject/wsgi.py
+++ firstproject/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for firstproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstproject.settings')
+sys.path.append('/opt/bitnami/apps/django/django_projects/bp')
+
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/bp/egg_cache")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firstproject.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
